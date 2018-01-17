b21fb74a9559c5df5b5eb315761fbe14326bce5f
niumoo <niumoo@126.com>
2018-01-17 10:58:06 +0800

    配置分文件管理

--- springmvc-mybatis/src/main/java/net/codingme/ssm/controller/JsonController.java
+++ springmvc-mybatis/src/main/java/net/codingme/ssm/controller/JsonController.java
@@ -1,11 +1,19 @@
 package net.codingme.ssm.controller;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import net.codingme.ssm.po.ItemsCustom;
+import net.codingme.ssm.po.User;
 
 /**
  * Description：
@@ -45,6 +53,34 @@ public class JsonController {
 	@RequestMapping("/responseJson")
 	public @ResponseBody ItemsCustom responseJson(ItemsCustom itemsCustom) {
 		return itemsCustom;
+	}
+	
+//	@RequestMapping("/queryJson")
+	@RequestMapping(value="/queryJson",method= {RequestMethod.POST})
+	public @ResponseBody Map queryJson() {
+		User user = new User();
+		user.setId(1);
+		user.setBirthday(new Date());
+		user.setSex("男");
+		user.setUsername("Niu");
+		user.setAddress("HFUU");
+		
+		User user2 = new User();
+		user2.setId(2);
+		user2.setBirthday(new Date());
+		user2.setSex("女");
+		user2.setUsername("Zxy");
+		user2.setAddress("HFUU");
+		ArrayList<User> arrayList = new ArrayList<User>();
+		arrayList.add(user);
+		arrayList.add(user2);
+		
+		Map<String, Serializable> map = new HashMap();
+		map.put("data", arrayList);
+		map.put("message", "查询成功");
+		map.put("code", 1);
+//		return user;
+		return map;
 	}
 	
 	
